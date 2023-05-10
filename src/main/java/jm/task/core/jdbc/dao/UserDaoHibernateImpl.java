f9b3f7c4dd7ba92868c50a2eea176b2c78a61161
Khamzat Guseynov <gamzat.guseynov.1999@mail.ru>
2023-05-10 15:53:56 +0300

    final

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.HibernateException;
 import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
@@ -15,9 +16,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
         Transaction transaction = null;
-        try (session) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.createNativeQuery("CREATE TABLE IF NOT EXISTS mysql.users" +
                     " (id mediumint not null auto_increment, name VARCHAR(25), " +
@@ -36,9 +36,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
         Transaction transaction = null;
-        try (session) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.createNativeQuery("DROP TABLE IF EXISTS mysql.users").executeUpdate();
             transaction.commit();
@@ -53,9 +52,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Session session = Util.getSessionFactory().openSession();
         Transaction transaction = null;
-        try (session) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             transaction.commit();
@@ -70,9 +68,8 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void removeUserById(long id) {
-        Session session = Util.getSessionFactory().openSession();
         Transaction transaction = null;
-        try (session) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.delete(session.get(User.class, id));
             transaction.commit();
@@ -87,26 +84,28 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public List<User> getAllUsers() {
-        Session session = Util.getSessionFactory().openSession();
-        CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
-        criteriaQuery.from(User.class);
-        Transaction transaction = session.beginTransaction();
-        List<User> userList = session.createQuery(criteriaQuery).getResultList();
-        try {
+        Transaction transaction = null;
+        List<User> userList = new ArrayList<>();
+        try (Session session = Util.getSessionFactory().openSession()){
+            CriteriaQuery<User> criteriaQuery = session.getCriteriaBuilder().createQuery(User.class);
+            criteriaQuery.from(User.class);
+            transaction = session.beginTransaction();
+            userList = session.createQuery(criteriaQuery).getResultList();
             transaction.commit();
             return userList;
         } catch (HibernateException e) {
             e.printStackTrace();
-            transaction.rollback();
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
         return userList;
     }
 
     @Override
     public void cleanUsersTable() {
-        Session session = Util.getSessionFactory().openSession();
         Transaction transaction = null;
-        try (session) {
+        try (Session session = Util.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
             session.createNativeQuery("TRUNCATE TABLE mysql.users;").executeUpdate();
             transaction.commit();
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,45 +9,52 @@ import java.util.List;
 public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS mysql.users" +
-                    "(id mediumint not null auto_increment," +
-                    "name VARCHAR(25)," +
-                    "lastname VARCHAR(25)," +
-                    "age tinyint," +
-                    "PRIMARY KEY (id))");
-            System.out.println("Таблица создана");
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("CREATE TABLE IF NOT EXISTS mysql.users" +
+                        "(id mediumint not null auto_increment," +
+                        "name VARCHAR(25)," +
+                        "lastname VARCHAR(25)," +
+                        "age tinyint," +
+                        "PRIMARY KEY (id))");
+                System.out.println("Таблица создана");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS mysql.users");
-            System.out.println("Таблица удалена");
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("DROP TABLE IF EXISTS mysql.users");
+                System.out.println("Таблица удалена");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement preparedStatement = Util.getConnection()
-                .prepareStatement("INSERT INTO mysql.users(name, lastname, age) VALUES(?,?,?)")) {
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, lastName);
-            preparedStatement.setByte(3, age);
-            preparedStatement.executeUpdate();
-            System.out.println("User с именем: " + name + " добавлен в базу данных");
+        try (Connection connection = Util.getConnection()) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO mysql.users(name, lastname, age) VALUES(?,?,?)")) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+                System.out.println("User с именем: " + name + " добавлен в базу данных");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
     public void removeUserById(long id) {
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.executeUpdate("DELETE FROM mysql.users WHERE id");
-            System.out.println("User удален");
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("DELETE FROM mysql.users WHERE id");
+                System.out.println("User удален");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -55,15 +62,17 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public List<User> getAllUsers() {
         List<User> allUser = new ArrayList<>();
-        try (Statement statement = Util.getConnection().createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age FROM mysql.users");
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                allUser.add(user);
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()){
+                ResultSet resultSet = statement.executeQuery("SELECT id, name, lastName, age FROM mysql.users");
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    allUser.add(user);
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -72,9 +81,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = Util.getConnection().createStatement()) {
-            statement.executeUpdate("TRUNCATE mysql.users");
-            System.out.println("Таблица очищена");
+        try (Connection connection = Util.getConnection()) {
+            try (Statement statement = connection.createStatement()){
+                statement.executeUpdate("TRUNCATE mysql.users");
+                System.out.println("Таблица очищена");
+            }
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Не удалось очистить");
